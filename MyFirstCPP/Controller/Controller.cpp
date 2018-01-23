ce75951e3db0527de2278d9d2bb138b76aeae71b
Dustin Schimel <dustin.schimel@gmail.com>
2018-01-23 12:46:07 -0700

    Added controller CPP and HPP files

---
+++ MyFirstCPP/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  MyFirstCPP
+//
+//  Created by Schimel, Dustin on 1/23/18.
+//  Copyright © 2018 Schimel, Dustin. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ MyFirstCPP/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  MyFirstCPP
+//
+//  Created by Schimel, Dustin on 1/23/18.
+//  Copyright © 2018 Schimel, Dustin. All rights reserved.
+//
+
+#ifndef Controller_hpp  //This means if it's never seen this before to define it
+#define Controller_hpp
+
+#include <iostream>
+
+class Controller
+{
+public:
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
